96b54d2092e93e99a249aa58d8cb12b87f37fa26
Olga Illarionova <ola.illari@gmail.com>
2020-11-11 19:29:51 -0800

    add get api/notes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,8 +1,17 @@
 const path = require('path');
+const fs = require('fs');
 
 module.exports = function(app){
     app.get('/api/notes', function(req,res){
+        ;
         console.log("reading notes");
+        fs.readFile(path.join(__dirname, "../db/db.json"),'utf8',(err,data)=>{
+            if (err) throw err;
+            console.log(data);
+            x = JSON.parse(data);
+            res.json(x);
+        })
+        //res.json(notes);
     });
     app.post('/api/notes', function(req,res){
         console.log("posting new note");
