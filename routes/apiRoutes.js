1a86b95537e7aa05ff8c66916d347d9b3a5ba4ea
Olga Illarionova <ola.illari@gmail.com>
2020-11-11 21:00:05 -0800

    post saves to file

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -8,16 +8,30 @@ module.exports = function(app){
         fs.readFile(path.join(__dirname, "../db/db.json"),'utf8',(err,data)=>{
             if (err) throw err;
             console.log(data);
-            x = JSON.parse(data);
+            let x = JSON.parse(data);
             res.json(x);
         })
         //res.json(notes);
     });
     app.post('/api/notes', function(req,res){
         console.log("posting new note");
+        console.log(req.body);
+        fs.readFile(path.join(__dirname, "../db/db.json"),'utf8',(err,data)=>{
+            if (err) throw err;
+            console.log(data);
+            let x = JSON.parse(data);
+            //res.json(x);
+            x.push(req.body);
+            console.log(x);
+            fs.writeFile(path.join(__dirname, "../db/db.json"),JSON.stringify(x), (err)=>{
+                if (err) throw err;
+                console.log("write something");
+            })
+        })
+
     });
     app.delete('/api/notes/:id',function(req,res){
-        console.log("deleting note");
+        console.log(`deleting note ${req.params.character}`);
     })
     
 }
